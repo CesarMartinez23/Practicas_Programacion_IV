13f978a23a83c4328df59c6277bb4a5349b7c73b
CesarMartinez23 <oliver-cesar@hotmail.com>
2023-01-31 22:20:42 -0600

    Practice Week 3 Added

---
+++ PracticaSemana3/src/views/exampleView.vue
@@ -0,0 +1,13 @@
+<template>
+  <example-component title="Programacion Computacional IV"></example-component>
+</template>
+
+<script>
+import exampleComponent from '@/components/exampleComponent.vue'
+export default {
+  name:"Cesar",
+  components:{
+      exampleComponent,
+  }
+}
+</script>
